7174f79cfad8e4f84e5cf80f097e7cd6fe322762
Christian <christianpnn_@hotmail.com>
2023-07-25 05:36:35 -0300

    Tipos de variáveis

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Tipos primitivos inteiros
+        short numeroPequeno = 10;
+        int numeroMedio = 1000;
+        long numeroGrande = 100000L;
+
+        //Tipos primitivos flutuantes
+        float hora = 3.45f;
+        double salarioMinimo = 2500.89;
+
+        //Tipo primitivo caractere
+        char sexo = 'f';
+
+        //Java é fortemente tipada e criar casting
+        short numeroCurto =  1;
+        int numeroNormal =  numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Criar uma constante
+        final double PI = 3.14;
+
+      
+
     }
 }
