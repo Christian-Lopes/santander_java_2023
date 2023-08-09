fa182a43939e93ea868409ba341e676d241bf61f
Christian <christianpnn_@hotmail.com>
2023-08-09 19:46:38 -0300

    Trabalhando com java doc

---
+++ javadoc/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora<h1>
+ * A calculadora realiza operações matemática entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes para 
+ * desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Christian Lopes
+ * @version 1.0
+ * @since 09/08/2023
+ */
+
+ public class Calculadora{
+    /**
+     * Este método é utilizado para dois númetos inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é soma dos dois números.
+     */
+
+     public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+     }
+ }
